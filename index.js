b8f4a6f0a1f9f98c30494b3d6ebdb7896e8c113d
Jordan Munoz <jordan@alveo.io>
2017-12-15 21:51:29 +0100

    d

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const cheerio = require('cheerio');
 
 
 
-const social_reason = 'bearstudio';
-const city = 'ROUEN';
+const social_reason = 'blablacar';
+const city = 'PARIS';
 
 
-requestp(`https://www.google.fr/search?q=${social_reason}+${city}`)
+requestp({
+	method:'GET',
+	url:`https://www.google.fr/search`,
+	qs: {q: `${city} ${social_reason}`},
+	headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'},
+	transform: (response) => cheerio.load(response),
+})
 	.then(response => {
-		response = response.body;
-		console.log(response);
+		const first = response('cite._Rm').first().text();
+		console.log(first);
 	});
