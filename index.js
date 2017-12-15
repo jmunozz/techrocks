ffb96f395d9720f0086e3b7c38e56a8231c0651c
Jordan Munoz <jordan@alveo.io>
2017-12-15 20:58:27 +0100

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+const request = require('request');
+const requestp =  require('request-promise');
+const cheerio = require('cheerio');
+
+
+
+const social_reason = 'bearstudio';
+const city = 'ROUEN';
+
+
+requestp({
+
+}).then(response => {
+	console.log(response);
+})
